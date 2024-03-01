75b7a0fdb281efaedd0742f8dab9617fb4c2ae7a
jungwoo <dlwjddn6520@gmail.com>
2024-03-01 15:01:25 +0900

    click event

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import React,{ useState }from 'react';
+
 function App() {
+  const [clicked, setClicked] = useState(false);
+
+  const btn_bk = () => {
+    setClicked(true);
+  };
+
+  const dynamicStyle = {
+    backgroundColor: clicked ? 'lightgreen' : 'white',
+  };
+
   return (
     <div className="container">
       <div className="question">
-          <div className='title'>
-            {/* 여기다가 문제 내용 넣을 거임 */}
-          </div>
+        <div className='title'>
+          {/* 여기다가 문제 내용 넣을 거임 */}
+        </div>
       </div>
 
       <div className="Choice">
         <div className='Choice_div'>
-          <label htmlFor="Choice1">문제 1: </label>
-          <input type="text" placeholder='뭐시꼥이' />
-          <label htmlFor="Choice2">문제 2: </label>
-          <input type="text" placeholder='뭐시꼥이' />
-          <label htmlFor="Choice3">문제 3: </label>
-          <input type="text" placeholder='뭐시꼥이' />
-          <label htmlFor="Choice4">문제 4: </label>
-          <input type="text" placeholder='뭐시꼥이' />
+          <div className='select' onClick={btn_bk} style={dynamicStyle}></div>
+          <div className='select' onClick={btn_bk} style={dynamicStyle}></div>
+          <div className='select' onClick={btn_bk} style={dynamicStyle}></div>
+          <div className='select' onClick={btn_bk} style={dynamicStyle}></div>
         </div>
       </div>
     </div>
